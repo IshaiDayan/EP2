ea3a318bd6ede7576c25bc306433d3533e78c355
IshaiDayan <ishaid@al.insper.edu.br>
2024-04-25 11:50:17 -0300

    input

--- principal.py
+++ principal.py
@@ -13,9 +13,9 @@ time.sleep(2)
 mapapc = funcoes_auxilio.cria_mapa(10)
 mapapc = funcoes_auxilio.aloca_navios(mapapc,list(constantes.PAISES[escolhadopc].values()))
 mapajg = funcoes_auxilio.cria_mapa(10)
-funcoes_auxilio.printar_mapas(mapapc,mapajg)
+#funcoes_auxilio.printar_mapas(mapapc,mapajg)
 print("O computador já está em posição de batalha!")
-time.sleep(1)
+time.sleep(2)
 for w in constantes.PAISES:
     print(" ")
     print ("\u001b[32m","  "+w,"\u001b[0m")
@@ -23,8 +23,9 @@ for w in constantes.PAISES:
     for q in constantes.PAISES[w]:
         espacos = " "*(12-len(q))
         print (q+espacos,':',constantes.PAISES[w][q])
-input("Qual a nação da sua frota?")
 
-
-
+escolhadojg = input("Qual a nação da sua frota?")
+print("Você escolheu o país {0}".format(escolhadojg))
+print("Agora é sua vez de alocar seus navios de guerra!")
+time.sleep(1)
 funcoes_auxilio.printar_mapas(mapapc,mapajg)
